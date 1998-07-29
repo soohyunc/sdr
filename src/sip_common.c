ae4e4814e0e456fdad556cd82ca8ac74c8219cfe
ucacewh <ucacewh@797363ac-862c-45c0-bc23-fe08f8bdc9f5>
1998-07-29 13:51:54 +0000

    Replaced ptr1=NULL+1 to just NULL - this code may need checking but at least it will build now

--- src/sip_common.c
+++ src/sip_common.c
@@ -750,8 +750,13 @@ int parse_sip_url(char *url, char* user, char *passwd, char *host,
     tag[0]='\0';
   if (transport!=NULL)
     *transport=SIP_NO_TRANSPORT;
+#ifdef WIN32
+  while (ptr1!=NULL)
+  {
+#else
   while (ptr1!=NULL+1)
   {
+#endif
     ptr2=strchr(ptr1, ';');
     if (ptr2!=NULL) *ptr2='\0';
     if ((strncmp(ptr1, "ttl=", 4)==0)&&(ttl!=NULL)) {
@@ -768,7 +773,12 @@ int parse_sip_url(char *url, char* user, char *passwd, char *host,
       strcat(others, ptr1);
       strcat(others, ";");
     } 
+#ifdef WIN32
+    if (ptr2==NULL)
+      ptr1=NULL;
+#else
     ptr1=ptr2+1;
+#endif
   }
   return 0;
     
