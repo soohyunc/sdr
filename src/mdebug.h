867d204d26b26f22dec39d4f6f5ded03c14ce4c8
mmaher <mmaher@797363ac-862c-45c0-bc23-fe08f8bdc9f5>
1999-06-16 02:33:49 +0000

    New - simple debug macro.

---
+++ src/mdebug.h
@@ -0,0 +1,19 @@
+#ifndef MM_DEBUG_H
+#define MM_DEBUG_H
+
+
+/* Debugging levels */
+#define SIP              0x00001
+#define REG              0x00002
+
+/* Debugging macros */
+#ifdef MDEBUG_FLAG
+extern int mdebug;
+#define MDEBUG(level, arg)         if (mdebug & level) printf/**/arg
+
+#else
+#define MDEBUG(level, arg)
+
+#endif // MDEBUG_FLAG
+
+#endif // MM_DEBUG_H
