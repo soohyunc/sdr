c3f2da39415bdd139e3948f5c3e65a37598e3b32
ucackha <ucackha@797363ac-862c-45c0-bc23-fe08f8bdc9f5>
1999-09-10 16:10:40 +0000

    disable pgp.  Kristian

--- src/sdr.tcl
+++ src/sdr.tcl
@@ -5,7 +5,7 @@
 #and for a DISCLAIMER OF ALL WARRANTIES.
 
 # set the following to 1 to disable enc/auth and 0 to enable it
-set pgpdisable  0
+set pgpdisable  1
 set x509disable 1
 
 set initWait 400
